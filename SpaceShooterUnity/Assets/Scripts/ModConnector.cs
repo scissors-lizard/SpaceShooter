7bb790d16355c48e07d74c48d1dfa39868d41da0
Josh Faubel <jfaubel3@gmail.com>
2018-04-12 22:30:25 -0400

    Joint snapping

---
+++ SpaceShooterUnity/Assets/Scripts/ModConnector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModConnector : MonoBehaviour {
+	public ModPiece parentPiece;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("trig");
+		Transform hitConnector = other.transform;
+		parentPiece.Snap (this, hitConnector);
+	}
+}
---
+++ SpaceShooterUnity/Assets/Scripts/ModPiece.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModPiece : MonoBehaviour {
+	private Transform hitConnector;
+	private ModConnector[] connectors;
+	private bool snapped = false;
+
+	void Awake (){
+		connectors = gameObject.GetComponentsInChildren<ModConnector> ();
+		for (int i = 0; i < connectors.Length; i++) {
+			connectors[i].parentPiece = this;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!snapped) {
+			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+		}
+	}
+
+
+
+//	void OnTriggerStay2D(Collider2D other)
+//	{
+//		otherHit = other;
+//	}
+//	void OnTriggerExit2D(Collider2D other)
+//	{
+//		otherHit = null;
+//	}
+//
+
+	public void Snap(ModConnector childConnector, Transform hitConnector){
+		this.hitConnector = hitConnector;
+		float angle = Quaternion.Angle (childConnector.transform.rotation, hitConnector.transform.rotation);
+		transform.rotation = transform.rotation * Quaternion.Euler(0f, angle + 180f, 0f);
+
+		Vector3 offset = hitConnector.transform.position - childConnector.transform.position;
+		transform.position += offset;
+		snapped = true;
+	}
+}
