7291288e1b197ede081d3404d1dba035e1752773
Josh Faubel <jfaubel3@gmail.com>
2018-04-13 00:06:23 -0400

    Modular piece snapping/ghosting

--- SpaceShooterUnity/Assets/Scripts/ModConnector.cs
+++ SpaceShooterUnity/Assets/Scripts/ModConnector.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class ModConnector : MonoBehaviour {
 	public ModPiece parentPiece;
 
+	[SerializeField] private SpriteRenderer spriteRend;
+
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		Debug.Log ("trig");
 		Transform hitConnector = other.transform;
 		parentPiece.Snap (this, hitConnector);
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		parentPiece.UnSnap ();
+	}
+
+	public void SetVisible(bool val){
+		spriteRend.enabled = val;
 	}
 }
--- SpaceShooterUnity/Assets/Scripts/ModPiece.cs
+++ SpaceShooterUnity/Assets/Scripts/ModPiece.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ModPiece : MonoBehaviour {
+	[SerializeField] private GameObject ghost;
+	[SerializeField] private GameObject[] visuals;
+
 	private Transform hitConnector;
 	private ModConnector[] connectors;
 	private bool snapped = false;
+	private Vector3 ghostPos;
+	private Quaternion ghostRot;
 
 	void Awake (){
 		connectors = gameObject.GetComponentsInChildren<ModConnector> ();
@@ -16,9 +21,12 @@ public class ModPiece : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (!snapped) {
-			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+
+		if (snapped) {
+			ghost.transform.position = ghostPos;
+			ghost.transform.rotation = ghostRot;
 		}
 	}
 
@@ -35,12 +43,51 @@ public class ModPiece : MonoBehaviour {
 //
 
 	public void Snap(ModConnector childConnector, Transform hitConnector){
+		// Reset ghost
+		ghost.gameObject.SetActive (true);
+		ghost.transform.position = transform.position;
+		ghost.transform.rotation = transform.rotation;
+//
+//		this.hitConnector = hitConnector;
+//		float angle = Quaternion.Angle (childConnector.transform.rotation, hitConnector.transform.rotation);
+//		transform.rotation = transform.rotation * Quaternion.Euler(0f, angle + 180f, 0f);
+//
+//		Vector3 offset = hitConnector.transform.position - childConnector.transform.position;
+//		transform.position += offset;
+//		snapped = true;
+//
+
 		this.hitConnector = hitConnector;
 		float angle = Quaternion.Angle (childConnector.transform.rotation, hitConnector.transform.rotation);
-		transform.rotation = transform.rotation * Quaternion.Euler(0f, angle + 180f, 0f);
+		ghost.transform.RotateAround(childConnector.transform.localPosition + ghost.transform.position, -Vector3.forward, angle + 180f);
 
 		Vector3 offset = hitConnector.transform.position - childConnector.transform.position;
-		transform.position += offset;
+		ghost.transform.position += offset;
+
+		ghostPos = ghost.transform.position;
+		ghostRot = ghost.transform.rotation;
 		snapped = true;
+
+		for (int i = 0; i < visuals.Length; i++) {
+			visuals [i].SetActive (false);
+		}
+		for (int i = 0; i < connectors.Length; i++) {
+			connectors [i].SetVisible (false);
+		}
+	}
+
+	public void UnSnap(){
+		// Reset ghost
+		ghost.gameObject.SetActive (false);
+
+		snapped = false;
+
+		for (int i = 0; i < visuals.Length; i++) {
+			visuals [i].SetActive (true);
+		}
+
+		for (int i = 0; i < connectors.Length; i++) {
+			connectors [i].SetVisible (true);
+		}
 	}
 }
