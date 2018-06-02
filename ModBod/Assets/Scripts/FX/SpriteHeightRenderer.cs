847eddf6867da119fa5e4508180eb22971271cd2
Josh Faubel <jfaubel3@gmail.com>
2018-06-02 01:11:09 -0400

    Fun with sprite stacking

---
+++ ModBod/Assets/Scripts/FX/SpriteHeightRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteHeightRenderer : MonoBehaviour {
+    public Transform baseTrans;
+    public int stackOrder = 0;
+    private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start ()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        sprite.sortingOrder = Mathf.RoundToInt(baseTrans.position.y * 1000f) * -1 + stackOrder;
+    }
+}
---
+++ ModBod/Assets/Scripts/FX/SpriteStack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteStack : MonoBehaviour {
+    [SerializeField] private int spriteCount;
+    [SerializeField] private Vector3 globalOffset;
+
+    private Transform[] sprites;
+    private SpriteRenderer myRend;
+
+	// Use this for initialization
+	void Start () {
+        sprites = new Transform[spriteCount];
+        myRend = GetComponent<SpriteRenderer>();
+
+        for (int i = 0; i < spriteCount; i++)
+        {
+            GameObject o = new GameObject(gameObject.name + "_s"+i);
+            o.transform.SetParent(transform);
+            SpriteHeightRenderer h = o.AddComponent<SpriteHeightRenderer>();
+            h.baseTrans = transform;
+            h.stackOrder = i;
+            SpriteRenderer r = o.GetComponent<SpriteRenderer>();
+            r.sprite = myRend.sprite;
+            sprites[i] = o.transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < spriteCount; i++)
+        {
+            sprites[i].transform.rotation = transform.rotation;
+            sprites[i].transform.position = transform.position + globalOffset * i;
+
+        }
+	}
+}
